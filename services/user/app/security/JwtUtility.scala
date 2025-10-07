018fd1736da388f06d112c8438d87498b8427974
Praanesh Selvaraj <praaneshselvaraj2003@gmail.com>
2025-10-07 10:28:10 +0530

    Code Formatting

--- services/user/app/security/JwtUtility.scala
+++ services/user/app/security/JwtUtility.scala
@@ -10,27 +10,29 @@ import javax.inject.{Inject, Singleton}
 import scala.util.Try
 
 @Singleton
-class JwtUtility @Inject()(configuration: Configuration) {
+class JwtUtility @Inject() (configuration: Configuration) {
 
   private val secretKey = configuration.get[String]("jwt.secretKey")
 
   private val expirationSeconds = configuration.get[Int]("jwt.expirationSecond")
 
-  private val algorithm = JwtAlgorithm.fromString(configuration.get[String]("jwt.algorithm")) match {
-    case algo: JwtHmacAlgorithm => algo
-    case _ => throw new IllegalArgumentException("Invalid JWT Algorithm")
-  }
+  private val algorithm =
+    JwtAlgorithm.fromString(configuration.get[String]("jwt.algorithm")) match {
+      case algo: JwtHmacAlgorithm => algo
+      case _                      => throw new IllegalArgumentException("Invalid JWT Algorithm")
+    }
 
   private implicit val clock: Clock = Clock.systemUTC
 
   def createToken(email: String, role: String): String = {
     val claim = JwtClaim(
-      content = Json.obj(
-        "email" -> email,
-        "role" -> role
-      ).toString()
-    )
-      .issuedNow
+      content = Json
+        .obj(
+          "email" -> email,
+          "role" -> role
+        )
+        .toString()
+    ).issuedNow
       .expiresIn(expirationSeconds)
 
     JwtJson.encode(claim, secretKey, algorithm)
@@ -46,6 +48,5 @@ class JwtUtility @Inject()(configuration: Configuration) {
     val role = (jsonContent \ "role").as[String]
     (email, role)
   }.toOption
-
 
 }
